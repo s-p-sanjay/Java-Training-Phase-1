bbed83a871d56a49c7dfac7951daf32a3466fac5
s-p-sanjay <snjprema9@gmail.com>
2025-02-11 17:51:05 +0530

    day 6

--- day6/Assignment2.java
+++ day6/Assignment2.java
@@ -31,7 +31,7 @@ public class Assignment2 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter a String : ");
-		checkVowels(in.next());
+		checkVowels(in.next().toLowerCase());
 
 	}
 
