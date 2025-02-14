bbd7bb358944a166fc2364f8846e201354a34428
s-p-sanjay <snjprema9@gmail.com>
2025-02-14 15:57:35 +0530

    Updated files

--- day8/ImageCopyExample.java
+++ day8/ImageCopyExample.java
@@ -9,7 +9,6 @@ public class ImageCopyExample {
 		FileInputStream fr = new FileInputStream("D:\\sanjay\\dhoni.jpg");
 		FileOutputStream fw = new FileOutputStream("D:\\sanjay\\dhoni1.jpg");
 		int ch = 0;
-		System.out.println(fr.read());
 		while((ch=fr.read()) != -1) {
 			fw.write(ch);
 		}
